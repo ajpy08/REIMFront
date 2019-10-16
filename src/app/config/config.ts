81eff69c0a57bd3b4d374dd51750d1884fe47837
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2019-10-16 18:05:37 -0500

    termino cambios de inventario

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,5 @@
 
-// export let URL_SERVICIOS = 'http://187.210.87.57:3000';
+ //export let URL_SERVICIOS = 'http://187.210.87.57:3000';
 
 export let URL_SERVICIOS = 'http://localhost:3000';
 
--- src/app/pages/maniobras/5inventario/inventario.component.ts
+++ src/app/pages/maniobras/5inventario/inventario.component.ts
@@ -18,32 +18,29 @@ export class InventarioComponent implements OnInit {
   cargando = true;
   totalRegistros: number = 0;
   totalRegistrosLR: number = 0;
-  // totalRegistros20: number = 0;
-  // totalRegistros40: number = 0;
   displayedColumns = ['fLlegada', 'viaje', 'nombre', 'fVigenciaTemporal', 'pdfTemporal', 'contenedor', 'tipo', 'peso', 'grado'];
-  displayedColumnsLR = ['naviera', 'contenedor', 'tipo', 'estado', 'cliente', 'aa', 'lavado', 'fotoslavado', 'reparaciones', 'fotosreparacion', 'grado'];
+  displayedColumnsLR = ['contenedor', 'tipo', 'peso', 'cliente', 'agencia', 'lavado', 'reparaciones', 'grado'];
   dataSource: any;
   dataSourceLR: any;
-  // dataSource20: any;
-  // dataSource40: any;
   c40: any;
   c20: any;
   groupedDisponibles20: any;
   groupedDisponibles40: any;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild('MatPaginatorLR', {read: MatPaginator}) MatPaginatorLR: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild("MatSort2") MatSort2: MatSort;
   constructor(public maniobraService: ManiobraService, private usuarioService: UsuarioService) { }
 
   ngOnInit() {
     this.usuarioLogueado = this.usuarioService.usuario;
-    //this.cargarInventario();
-    this.cargarLR();
+    this.cargarInventario();
 
     if (this.usuarioLogueado.role == ROLES.ADMIN_ROLE || this.usuarioLogueado.role == ROLES.REIMADMIN_ROLE) {
-      this.displayedColumnsLR = ['actions', 'naviera', 'contenedor', 'tipo', 'estado', 'cliente', 'aa', 'lavado', 'fotoslavado', 'reparaciones', 'fotosreparacion', 'grado'];
+      this.displayedColumnsLR = ['actions', 'contenedor', 'tipo', 'peso', 'cliente', 'agencia', 'lavado', 'reparaciones', 'grado'];
     } else {
-      this.displayedColumnsLR = ['naviera', 'contenedor', 'tipo', 'estado', 'cliente', 'aa', 'lavado', 'fotoslavado', 'reparaciones', 'fotosreparacion', 'grado'];
+      this.displayedColumnsLR = ['contenedor', 'tipo', 'peso', 'cliente', 'agencia', 'lavado', 'reparaciones', 'grado'];
     }
   }
 
@@ -56,85 +53,134 @@ export class InventarioComponent implements OnInit {
 
   cargarInventario() {
     this.cargando = true;
-    this.maniobraService.getManiobras('D', ETAPAS_MANIOBRA.DISPONIBLE)
-      .subscribe(maniobras => {
+    if (this.usuarioLogueado.empresas.length > 0) {
+      this.maniobraService.getManiobras('D', ETAPAS_MANIOBRA.DISPONIBLE, null, null, null, null, null, null, this.usuarioLogueado.empresas[0]._id)
+        .subscribe(maniobras => {
 
-        this.c20 = maniobras.maniobras.filter(m => m.tipo.includes('20'));
+          this.c20 = maniobras.maniobras.filter(m => m.tipo.includes('20'));
 
-        const grouped20 = this.c20.reduce((curr, m) => {
-          if (!curr[m.tipo]) {
-            // Si no has tenido ninguna entrada de ese tipo la agregas pero usando un arreglo
-            curr[m.tipo] = [m];
-          } else {
-            // Si ya tienes ese tipo lo agregas al final del arreglo
-            curr[m.tipo].push(m);
-          }
-          return curr;
-        }, {});
+          const grouped20 = this.c20.reduce((curr, m) => {
+            if (!curr[m.tipo]) {
+              // Si no has tenido ninguna entrada de ese tipo la agregas pero usando un arreglo
+              curr[m.tipo] = [m];
+            } else {
+              // Si ya tienes ese tipo lo agregas al final del arreglo
+              curr[m.tipo].push(m);
+            }
+            return curr;
+          }, {});
 
-        // Luego conviertes ese objeto en un arreglo que *ngFor puede iterar
-        this.groupedDisponibles20 = Object.keys(grouped20).map(tipo => {
-          return {
-            tipo: tipo,
-            maniobras: grouped20[tipo]
-          };
+          // Luego conviertes ese objeto en un arreglo que *ngFor puede iterar
+          this.groupedDisponibles20 = Object.keys(grouped20).map(tipo => {
+            return {
+              tipo: tipo,
+              maniobras: grouped20[tipo]
+            };
+          });
+
+          this.c40 = maniobras.maniobras.filter(m => m.tipo.includes('40'));
+
+          const grouped40 = this.c40.reduce((curr, m) => {
+            if (!curr[m.tipo]) {
+              // Si no has tenido ninguna entrada de ese tipo la agregas pero usando un arreglo
+              curr[m.tipo] = [m];
+            } else {
+              // Si ya tienes ese tipo lo agregas al final del arreglo
+              curr[m.tipo].push(m);
+            }
+            return curr;
+          }, {});
+
+          // Luego conviertes ese objeto en un arreglo que *ngFor puede iterar
+          this.groupedDisponibles40 = Object.keys(grouped40).map(tipo => {
+            return {
+              tipo: tipo,
+              maniobras: grouped40[tipo]
+            };
+          });
+
+          this.dataSource = new MatTableDataSource(maniobras.maniobras);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+          this.totalRegistros = maniobras.maniobras.length;
         });
+      this.cargando = false;
+    } else {
+      this.maniobraService.getManiobras('D', ETAPAS_MANIOBRA.DISPONIBLE)
+        .subscribe(maniobras => {
 
-        this.c40 = maniobras.maniobras.filter(m => m.tipo.includes('40'));
+          this.c20 = maniobras.maniobras.filter(m => m.tipo.includes('20'));
 
-        const grouped40 = this.c40.reduce((curr, m) => {
-          if (!curr[m.tipo]) {
-            // Si no has tenido ninguna entrada de ese tipo la agregas pero usando un arreglo
-            curr[m.tipo] = [m];
-          } else {
-            // Si ya tienes ese tipo lo agregas al final del arreglo
-            curr[m.tipo].push(m);
-          }
-          return curr;
-        }, {});
+          const grouped20 = this.c20.reduce((curr, m) => {
+            if (!curr[m.tipo]) {
+              // Si no has tenido ninguna entrada de ese tipo la agregas pero usando un arreglo
+              curr[m.tipo] = [m];
+            } else {
+              // Si ya tienes ese tipo lo agregas al final del arreglo
+              curr[m.tipo].push(m);
+            }
+            return curr;
+          }, {});
 
-        // Luego conviertes ese objeto en un arreglo que *ngFor puede iterar
-        this.groupedDisponibles40 = Object.keys(grouped40).map(tipo => {
-          return {
-            tipo: tipo,
-            maniobras: grouped40[tipo]
-          };
+          // Luego conviertes ese objeto en un arreglo que *ngFor puede iterar
+          this.groupedDisponibles20 = Object.keys(grouped20).map(tipo => {
+            return {
+              tipo: tipo,
+              maniobras: grouped20[tipo]
+            };
+          });
+
+          this.c40 = maniobras.maniobras.filter(m => m.tipo.includes('40'));
+
+          const grouped40 = this.c40.reduce((curr, m) => {
+            if (!curr[m.tipo]) {
+              // Si no has tenido ninguna entrada de ese tipo la agregas pero usando un arreglo
+              curr[m.tipo] = [m];
+            } else {
+              // Si ya tienes ese tipo lo agregas al final del arreglo
+              curr[m.tipo].push(m);
+            }
+            return curr;
+          }, {});
+
+          // Luego conviertes ese objeto en un arreglo que *ngFor puede iterar
+          this.groupedDisponibles40 = Object.keys(grouped40).map(tipo => {
+            return {
+              tipo: tipo,
+              maniobras: grouped40[tipo]
+            };
+          });
+
+          this.dataSource = new MatTableDataSource(maniobras.maniobras);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+          this.totalRegistros = maniobras.maniobras.length;
         });
-
-
-        this.dataSource = new MatTableDataSource(maniobras.maniobras);
-        this.dataSource.sort = this.sort;
-        this.dataSource.paginator = this.paginator;
-        this.totalRegistros = maniobras.maniobras.length;
-      });
-
-      this.cargarLR();
-      
-    this.cargando = false;
+      this.cargando = false;
+    }
+    this.cargarLR();
   }
 
-  cargarLR(){
+  cargarLR() {
     if (this.usuarioLogueado.empresas.length > 0) {
       this.cargando = true;
-      this.maniobraService.getManiobrasConLavadoReparacion(this.usuarioLogueado.empresas[0]._id,
-        '', '', '', '')
+      this.maniobraService.getManiobrasNaviera(ETAPAS_MANIOBRA.LAVADO_REPARACION, this.usuarioLogueado.empresas[0]._id)
         .subscribe(maniobras => {
           this.dataSourceLR = new MatTableDataSource(maniobras.maniobras);
-          this.dataSourceLR.sort = this.sort;
-          this.dataSourceLR.paginator = this.paginator;
-          this.totalRegistrosLR = maniobras.maniobras.length;
+          this.dataSourceLR.sort = this.MatSort2;
+          this.dataSourceLR.paginator = this.MatPaginatorLR;
+          this.totalRegistrosLR = maniobras.total;
         });
       this.cargando = false;
     } else {
       if (this.usuarioLogueado.role == ROLES.ADMIN_ROLE || this.usuarioLogueado.role == ROLES.REIMADMIN_ROLE) {
         this.cargando = true;
-        this.maniobraService.getManiobrasConLavadoReparacion(null,
-          '', '', '', '')
+        this.maniobraService.getManiobrasNaviera(ETAPAS_MANIOBRA.LAVADO_REPARACION)
           .subscribe(maniobras => {
             this.dataSourceLR = new MatTableDataSource(maniobras.maniobras);
-            this.dataSourceLR.sort = this.sort;
-            this.dataSourceLR.paginator = this.paginator;
-            this.totalRegistrosLR = maniobras.maniobras.length;
+            this.dataSourceLR.sort = this.MatSort2;
+            this.dataSourceLR.paginator = this.MatPaginatorLR;
+            this.totalRegistrosLR = maniobras.total;
           });
         this.cargando = false;
       }
