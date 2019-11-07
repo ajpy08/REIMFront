c9f0ece0c07cc3dc5b5050b440427d6c0bb099c6
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2019-11-07 08:14:35 -0600

    descargas

--- src/app/pages/solicitudes/aprobaciones.component.ts
+++ src/app/pages/solicitudes/aprobaciones.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Solicitud } from './solicitud.models';
 import { SolicitudService } from '../../services/service.index';
-import { MatTabGroup, MatTabChangeEvent, MatTab } from '@angular/material';
+import { MatTabGroup, MatTabChangeEvent, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 
 declare var swal: any;
@@ -14,10 +14,23 @@ declare var swal: any;
 export class AprobacionesComponent implements OnInit {
   @ViewChild(MatTabGroup) tabGroup: MatTabGroup;
 
-  solicitudes: any[] = [];
-  solicitudesCarga: any[] = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator; //descargas
+  @ViewChild('pagCargas', { read: MatPaginator }) pagCargas: MatPaginator; //cargas
+  
+  @ViewChild(MatSort) sort: MatSort; //descargas
+  @ViewChild('sortCargas') sortEspera: MatSort; //cargas
+
   cargando = true;
-  totalRegistros = 0;
+
+  displayedColumnsCarga = ['actions', 'fAlta', 'tipo', 'agencia.nombreComercial', 'naviera.nombreComercial', 'cliente.nombreComercial', 'viaje.viaje', 'buque.nombre',
+    'observaciones', 'estatus'];
+
+  displayedColumnsDescarga = ['actions', 'fAlta', 'tipo', 'agencia.nombreComercial', 'naviera.nombreComercial', 'cliente.nombreComercial', 'viaje.viaje', 'buque.nombre',
+    'observaciones', 'estatus'];
+
+  dtCargas: any;
+  dtDescargas: any;
+  totalRegistrosDescargas = 0;
   totalRegistrosCargas = 0;
 
   constructor(public _solicitudesService: SolicitudService) { }
@@ -25,28 +38,57 @@ export class AprobacionesComponent implements OnInit {
   ngOnInit() {
     this.cargaSolicitudes();
     let indexTAB = localStorage.getItem("AprobSolicitudes");
-    if (indexTAB) { 
-          this.tabGroup.selectedIndex = Number.parseInt(indexTAB);
-      }
-    
-
+    if (indexTAB) {
+      this.tabGroup.selectedIndex = Number.parseInt(indexTAB);
+    }
   }
 
   cargaSolicitudes() {
     this.cargando = true;
     this._solicitudesService.getSolicitudes('D')
-    .subscribe(resp => {
-      this.totalRegistros = resp.total;
-      this.solicitudes = resp.solicitudes;
-      this.cargando = false;
-    });
+      .subscribe(resp => {
+
+        this.dtDescargas = new MatTableDataSource(resp.solicitudes);
+        this.dtDescargas.sortingDataAccessor = (item, property) => {
+          if (property.includes('.')) return property.split('.').reduce((o, i) => o ? o[i] : undefined, item)
+          return item[property];
+        };
+        this.dtDescargas.sort = this.sort;
+        this.dtDescargas.paginator = this.paginator;
+        this.totalRegistrosDescargas = resp.total;
+        //this.dtDescargas.filterPredicate  = this.Filtro();
+        this.cargando = false;
+      });
     this.cargando = true;
     this._solicitudesService.getSolicitudes('C')
-    .subscribe(resp => {
-      this.totalRegistrosCargas = resp.total;
-      this.solicitudesCarga = resp.solicitudes;
-      this.cargando = false;
-    });
+      .subscribe(resp => {
+
+        this.dtCargas = resp.solicitudes;
+        // this.dtCargas = new MatTableDataSource(resp.solicitudes);
+        // this.dtCargas.sortingDataAccessor = (item, property) => {
+        //   if (property.includes('.')) return property.split('.').reduce((o, i) => o ? o[i] : undefined, item)
+        //   return item[property];
+        // };
+        // this.dtCargas.sort = this.sortCargas;
+        // this.dtCargas.paginator = this.pagCargas;
+        this.totalRegistrosCargas = resp.total;
+        //this.dtCargas.filterPredicate  = this.Filtro();
+        this.cargando = false;
+      });
+  }
+
+  applyFilterCargas(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dtCargas.filter = filterValue;
+    this.totalRegistrosCargas = this.dtCargas.filteredData.length;
+  }
+
+  applyFilterDescargas(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dtDescargas.filter = filterValue;
+    this.totalRegistrosDescargas = this.dtDescargas.filteredData.length;
   }
 
   onLinkClick(event: MatTabChangeEvent) {
@@ -54,19 +96,30 @@ export class AprobacionesComponent implements OnInit {
     localStorage.setItem("AprobSolicitudes", event.index.toString());
   }
 
-  borrarSolicitud( sol: Solicitud ) {
-    swal({title: '¿Esta seguro?', text: 'Esta apunto de borrar la solicitud.' , icon: 'warning', buttons: true, dangerMode: true, })
+  borrarSolicitud(sol: Solicitud) {
+    swal({ title: '¿Esta seguro?', text: 'Esta apunto de borrar la solicitud.', icon: 'warning', buttons: true, dangerMode: true, })
       .then(borrar => {
         if (borrar) {
           this._solicitudesService.borrarSolicitud(sol._id)
-          .subscribe(borrado => {
-            this.cargaSolicitudes();
-          });
+            .subscribe(borrado => {
+              this.cargaSolicitudes();
+            });
         }
       });
   }
 
-
-
-
+  Filtro(): (data: any, filter: string) => boolean {
+    let filterFunction = function (data, filter): boolean {
+      const dataStr = data.contenedor.toLowerCase() +
+        (data.folio ? data.folio : '') +
+        data.tipo.toLowerCase() +
+        data.peso.toLowerCase() +
+        (data.viaje ? data.viaje.viaje.toLowerCase() : '') +
+        (data.cliente ? data.cliente.nombreComercial.toLowerCase() : '') +
+        (data.viaje ? data.viaje.buque.nombre.toLowerCase() : '') +
+        (data.agencia ? data.agencia.nombreComercial.toLowerCase() : '');
+      return dataStr.indexOf(filter) != -1;
+    }
+    return filterFunction;
+  }
 }
