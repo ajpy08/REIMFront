45dcf86dd41e1bc1e474c688df35a02f47870d5e
RicGH <34562464+RicGH@users.noreply.github.com>
2019-12-04 16:36:36 -0600

    se corrige cambio de error

--- src/app/pages/solicitudes/descarga/solicitud_descarga.component.ts
+++ src/app/pages/solicitudes/descarga/solicitud_descarga.component.ts
@@ -443,7 +443,7 @@ export class SolicitudDescargaComponent implements OnInit {
   }
 
   onFilePDFBLSelected(event) {
-    this.fileBL = <File> event.target.files[0];
+    this.fileBL = <File> event.target.files[0]; 
     this.subirBL();
   }
 
