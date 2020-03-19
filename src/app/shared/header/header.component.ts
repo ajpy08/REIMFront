b82ef13d5fbe1dfe2f6c4233017ea5c7044e7340
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-03-19 12:40:59 -0600

    Temp notificaciones

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,12 @@
+import { Notification } from './../../models/notification.models';
+import { SolicitudService } from './../../pages/solicitudes/solicitud.service';
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../services/service.index';
 import { Usuario } from '../../pages/usuarios/usuario.model';
 import { Router } from '@angular/router';
 import { URL_SOCKET_IO, PARAM_SOCKET } from '../../../environments/environment';
 import * as io from 'socket.io-client';
+import { ROLES } from 'src/app/config/config';
 
 @Component({
   selector: 'app-header',
@@ -13,18 +16,23 @@ import * as io from 'socket.io-client';
 export class HeaderComponent implements OnInit {
 
   usuario: Usuario;
-  ultimos = [];
-  socket = io(URL_SOCKET_IO, PARAM_SOCKET);
+  notifications: Notification[] = [];
+  socket = io(URL_SOCKET_IO, PARAM_SOCKET, PARAM_SOCKET);
 
   constructor(public _usuarioService: UsuarioService,
+    public solicitudesService: SolicitudService,
     public router: Router) { }
 
   ngOnInit() {
     this.usuario = this._usuarioService.usuario;
 
-    // this.socket.on('new-alert', function (data: any) {
-    //   this.ultimos.push(data.data);
-    // }.bind(this));
+    this.socket.on('new-solicitud', function (data: any) {
+      if (this.usuario.role === ROLES.ADMIN_ROLE || this.usuario.role === ROLES.PATIOADMIN_ROLE) {
+        this.solicitudesService.getSolicitudes('D', 'NA').subscribe(resp => {
+          this.notifications = this.doSolicitudesNotifications(resp.solicitudes);
+        });
+      }
+    }.bind(this));
   }
 
   buscar(termino: string) {
@@ -37,6 +45,21 @@ export class HeaderComponent implements OnInit {
     console.log(today);
     const minutesAgo = 1;
     return fAlta;
+  }
+
+  doSolicitudesNotifications(solicitudes) {
+    const notificationsTemp: Notification[] = [];
+    solicitudes.forEach(solicitud => {
+      const notify = new Notification;
+      const tipo = solicitud.tipo === 'D' ? 'Descarga' : solicitud.tipo === 'C' ? 'Carga' : 'TIPO';
+      notify.name = solicitud.agencia;
+      notify.description = `Solicitud de ${tipo} (${solicitud.contenedores.length} contenedores)`;
+      notify.fAlta = solicitud.fAlta;
+      notify._id = solicitud._id;
+
+      notificationsTemp.push(notify);
+    });
+    return notificationsTemp;
   }
 
   logout() {
