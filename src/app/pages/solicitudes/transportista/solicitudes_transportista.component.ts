0f560a571f6fc3f97c0abbaf96c0ff4a1ff1547d
RicGH <34562464+RicGH@users.noreply.github.com>
2020-03-24 10:45:15 -0600

    termino papeleta soket

--- src/app/pages/solicitudes/transportista/solicitudes_transportista.component.ts
+++ src/app/pages/solicitudes/transportista/solicitudes_transportista.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {
   ManiobraService,
   UsuarioService
-} from "../../../services/service.index";
+} from '../../../services/service.index';
 import {
   MatTableDataSource,
   MatSort,
   MatPaginator,
   MatTabGroup,
   MatTabChangeEvent
-} from "@angular/material";
-import { ROLES } from "src/app/config/config";
-
+} from '@angular/material';
+import { ROLES } from 'src/app/config/config';
+import { URL_SOCKET_IO, PARAM_SOCKET } from '../../../../environments/environment';
+import * as io from 'socket.io-client';
+import swal from 'sweetalert';
 @Component({
-  selector: "app-solicitudes.transportista",
-  templateUrl: "./solicitudes_transportista.component.html",
+  selector: 'app-solicitudes.transportista',
+  templateUrl: './solicitudes_transportista.component.html',
   styles: [],
   providers: []
 })
@@ -25,30 +27,30 @@ export class SolicitudesTransportistaComponent implements OnInit {
   totalRegistros = 0;
   totalRegistrosCargas = 0;
   usuarioLogueado: any;
-
+  socket = io(URL_SOCKET_IO, PARAM_SOCKET);
   displayedDescargasColumns = [
-    "actions",
-    "folio",
-    "contenedor",
-    "tipo",
-    "peso",
-    "cliente",
-    "agencia",
-    "operador",
-    "camion"
+    'actions',
+    'folio',
+    'contenedor',
+    'tipo',
+    'peso',
+    'cliente',
+    'agencia',
+    'operador',
+    'camion'
   ];
   dataSourceDescargas: any;
 
   displayedCargasColumns = [
-    "actions",
-    "folio",
-    "contenedor",
-    "tipo",
-    "peso",
-    "cliente",
-    "agencia",
-    "operador",
-    "camion"
+    'actions',
+    'folio',
+    'contenedor',
+    'tipo',
+    'peso',
+    'cliente',
+    'agencia',
+    'operador',
+    'camion'
   ];
   dataSourceCargas: any;
 
@@ -56,9 +58,9 @@ export class SolicitudesTransportistaComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  @ViewChild("MatPaginatorCarga", { read: MatPaginator })
+  @ViewChild('MatPaginatorCarga', { read: MatPaginator })
   MatPaginatorCarga: MatPaginator;
-  @ViewChild("MatSortCarga") MatSortCarga: MatSort;
+  @ViewChild('MatSortCarga') MatSortCarga: MatSort;
 
   constructor(
     private _maniobraService: ManiobraService,
@@ -67,96 +69,113 @@ export class SolicitudesTransportistaComponent implements OnInit {
 
   ngOnInit() {
     this.usuarioLogueado = this._usuarioService.usuario;
-    this.cargarManiobras();
-    let indexTAB = localStorage.getItem("TrasportistaTabs");
+    this.cargarManiobras('D');
+    this.cargarManiobras('C');
+    const indexTAB = localStorage.getItem('TrasportistaTabs');
     if (indexTAB) {
+      // tslint:disable-next-line: radix
       this.tabGroup.selectedIndex = Number.parseInt(indexTAB);
     }
+
+    this.socket.on('update-papeleta', function(data: any){
+      if ((this.usuarioLogueado.role === ROLES.ADMIN_ROLE || this.usuarioLogueado.role === ROLES.PATIOADMIN_ROLE) ||
+      (data.data.transportista === this.usuarioLogueado.empresas[0]._id && this.usuarioLogueado.role === ROLES.TRANSPORTISTA_ROLE)) {
+        if (data.data._id) {
+          this.cargarManiobras(data.data.cargaDescarga);
+        }
+      }
+    }.bind(this));
   }
 
-  cargarManiobras() {
+  cargarManiobras(CD: string) {
     this.cargando = true;
-    if (this.usuarioLogueado.role === "TRANSPORTISTA_ROLE") {
-      this._maniobraService
-        .getManiobras(
-          "D",
-          "TRANSITO",
-          this.usuarioLogueado.empresas[0]._id,
-          null,
-          null,
-          "VACIO_IMPORT,LLENO_IMPORT,LLENO_EXPORT"
-        )
-        .subscribe(maniobras => {
-          // this.totalRegistros = maniobras.total;
-          // this.maniobras = maniobras.maniobras;
-          this.dataSourceDescargas = new MatTableDataSource(
-            maniobras.maniobras
-          );
-          this.dataSourceDescargas.sort = this.sort;
-          this.dataSourceDescargas.paginator = this.paginator;
-          this.totalRegistros = maniobras.total;
-        });
-      this._maniobraService
-        .getManiobras(
-          "C",
-          "TRANSITO",
-          this.usuarioLogueado.empresas[0]._id,
-          null,
-          null,
-          "VACIO_EXPORT,LLENO_IMPORT,LLENO_EXPORT"
-        )
-        .subscribe(maniobras => {
-          // this.totalRegistrosCargas = maniobras.total;
-          // this.maniobrasCarga = maniobras.maniobras;
-          this.dataSourceCargas = new MatTableDataSource(maniobras.maniobras);
-          this.dataSourceCargas.sort = this.MatSortCarga;
-          this.dataSourceCargas.paginator = this.MatPaginatorCarga;
-          this.totalRegistrosCargas = maniobras.total;
-        });
-      this.cargando = false;
+    if (this.usuarioLogueado.role === 'TRANSPORTISTA_ROLE') {
+      if (CD === 'D') {
+        this._maniobraService
+          .getManiobras(
+            'D',
+            'TRANSITO',
+            this.usuarioLogueado.empresas[0]._id,
+            null,
+            null,
+            'VACIO_IMPORT,LLENO_IMPORT,LLENO_EXPORT'
+          )
+          .subscribe(maniobras => {
+            // this.totalRegistros = maniobras.total;
+            // this.maniobras = maniobras.maniobras;
+            this.dataSourceDescargas = new MatTableDataSource(
+              maniobras.maniobras
+            );
+            this.dataSourceDescargas.sort = this.sort;
+            this.dataSourceDescargas.paginator = this.paginator;
+            this.totalRegistros = maniobras.total;
+          });
+      } else if (CD === 'C') {
+        this._maniobraService
+          .getManiobras(
+            'C',
+            'TRANSITO',
+            this.usuarioLogueado.empresas[0]._id,
+            null,
+            null,
+            'VACIO_EXPORT,LLENO_IMPORT,LLENO_EXPORT'
+          )
+          .subscribe(maniobras => {
+            // this.totalRegistrosCargas = maniobras.total;
+            // this.maniobrasCarga = maniobras.maniobras;
+            this.dataSourceCargas = new MatTableDataSource(maniobras.maniobras);
+            this.dataSourceCargas.sort = this.MatSortCarga;
+            this.dataSourceCargas.paginator = this.MatPaginatorCarga;
+            this.totalRegistrosCargas = maniobras.total;
+          });
+        this.cargando = false;
+      }
     }
 
     if (
-      this.usuarioLogueado.role == ROLES.ADMIN_ROLE ||
-      this.usuarioLogueado.role == ROLES.PATIOADMIN_ROLE
+      this.usuarioLogueado.role === ROLES.ADMIN_ROLE ||
+      this.usuarioLogueado.role === ROLES.PATIOADMIN_ROLE
     ) {
-      this._maniobraService
-        .getManiobras(
-          "D",
-          "TRANSITO",
-          null,
-          null,
-          null,
-          "VACIO_IMPORT,LLENO_IMPORT,LLENO_EXPORT"
-        )
-        .subscribe(maniobras => {
-          // this.totalRegistros = maniobras.total;
-          // this.maniobras = maniobras.maniobras;
-          this.dataSourceDescargas = new MatTableDataSource(
-            maniobras.maniobras
-          );
-          this.dataSourceDescargas.sort = this.sort;
-          this.dataSourceDescargas.paginator = this.paginator;
-          this.totalRegistros = maniobras.total;
-        });
-      this._maniobraService
-        .getManiobras(
-          "C",
-          "TRANSITO",
-          null,
-          null,
-          null,
-          "VACIO_EXPORT,LLENO_IMPORT,LLENO_EXPORT"
-        )
-        .subscribe(maniobras => {
-          // this.totalRegistrosCargas = maniobras.total;
-          // this.maniobrasCarga = maniobras.maniobras;
-          this.dataSourceCargas = new MatTableDataSource(maniobras.maniobras);
-          this.dataSourceCargas.sort = this.MatSortCarga;
-          this.dataSourceCargas.paginator = this.MatPaginatorCarga;
-          this.totalRegistrosCargas = maniobras.total;
-        });
-      this.cargando = false;
+      if (CD === 'D') {
+        this._maniobraService
+          .getManiobras(
+            'D',
+            'TRANSITO',
+            null,
+            null,
+            null,
+            'VACIO_IMPORT,LLENO_IMPORT,LLENO_EXPORT'
+          )
+          .subscribe(maniobras => {
+            // this.totalRegistros = maniobras.total;
+            // this.maniobras = maniobras.maniobras;
+            this.dataSourceDescargas = new MatTableDataSource(
+              maniobras.maniobras
+            );
+            this.dataSourceDescargas.sort = this.sort;
+            this.dataSourceDescargas.paginator = this.paginator;
+            this.totalRegistros = maniobras.total;
+          });
+      } else if (CD === 'C') {
+        this._maniobraService
+          .getManiobras(
+            'C',
+            'TRANSITO',
+            null,
+            null,
+            null,
+            'VACIO_EXPORT,LLENO_IMPORT,LLENO_EXPORT'
+          )
+          .subscribe(maniobras => {
+            // this.totalRegistrosCargas = maniobras.total;
+            // this.maniobrasCarga = maniobras.maniobras;
+            this.dataSourceCargas = new MatTableDataSource(maniobras.maniobras);
+            this.dataSourceCargas.sort = this.MatSortCarga;
+            this.dataSourceCargas.paginator = this.MatPaginatorCarga;
+            this.totalRegistrosCargas = maniobras.total;
+          });
+        this.cargando = false;
+      }
     }
   }
 
@@ -168,7 +187,7 @@ export class SolicitudesTransportistaComponent implements OnInit {
       this.dataSourceDescargas.filter = filterValue;
       this.totalRegistros = this.dataSourceDescargas.filteredData.length;
     } else {
-      console.error("Error al filtrar el dataSource de Solicitudes Transportista Descargas");
+      console.error('Error al filtrar el dataSource de Solicitudes Transportista Descargas');
     }
 
     // this.dataSourceDescargas.filter = filterValue;
@@ -176,7 +195,7 @@ export class SolicitudesTransportistaComponent implements OnInit {
   }
 
   onLinkClick(event: MatTabChangeEvent) {
-    localStorage.setItem("TrasportistaTabs", event.index.toString());
+    localStorage.setItem('TrasportistaTabs', event.index.toString());
   }
 
   applyFilterCargas(filterValue: string) {
@@ -187,7 +206,7 @@ export class SolicitudesTransportistaComponent implements OnInit {
       this.dataSourceCargas.filter = filterValue;
       this.totalRegistrosCargas = this.dataSourceCargas.filteredData.length;
     } else {
-      console.error("Error al filtrar el dataSource de Solicitudes Transportista Cargas");
+      console.error('Error al filtrar el dataSource de Solicitudes Transportista Cargas');
     }
 
     // this.dataSourceCargas.filter = filterValue;
