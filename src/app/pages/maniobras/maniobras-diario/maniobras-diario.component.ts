9b5aa827626f47e785962390c4c363bbeb703e82
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-02-16 08:01:46 -0600

    agrego campo a maniobras diario

--- src/app/pages/maniobras/maniobras-diario/maniobras-diario.component.ts
+++ src/app/pages/maniobras/maniobras-diario/maniobras-diario.component.ts
@@ -346,7 +346,8 @@ export class ManiobrasDiarioComponent implements OnInit {
             ? m.viaje.naviera.nombreComercial
             : '' && m.viaje.naviera.nombreComercial,
         Peso: m.peso,
-        Estatus: m.estatus
+        Estatus: m.estatus,
+        Factura: m.facturaManiobra
         // LavadoObservacion: m.lavadoObservacion,
         // Reparaciones: reparaciones,
         // ReparacionesObservacion: m.reparacionesObservacion,
